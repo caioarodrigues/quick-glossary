d85b78a3fb5e85755e31ff07b5ae33418c34c553
Caioã Rodrigues <caioarodrigues@alu.ufc.br>
2025-03-26 12:29:06 -0300

    fix: TypeScript erros

--- src/presentation/components/SearchResult.tsx
+++ src/presentation/components/SearchResult.tsx
@@ -1,5 +1,17 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useSearch } from "../hooks/useSearch";
+
+interface Definition {
+  definition: string;
+}
+
+interface Meaning {
+  definitions: Definition[];
+}
+
+interface Word {
+  word: string;
+  meanings: Meaning[];
+}
 
 export function SearchResult() {
   const { searchContextValue } = useSearch();
@@ -16,25 +28,27 @@ export function SearchResult() {
           searchContextValue && searchContextValue.length > 0 ? "p-2" : ""
         }`}
       >
-        {searchContextValue?.map(({ word, meanings }, wordIndex) => (
+        {searchContextValue?.map(({ meanings }: Word, wordIndex: number) => (
           <div key={wordIndex}>
-            {meanings.map(({ definitions }, meaningIndex) => (
+            {meanings.map(({ definitions }: Meaning, meaningIndex: number) => (
               <div key={meaningIndex}>
-                {definitions.map(({ definition }, definitionIndex) => (
-                  <div
-                    key={definitionIndex}
-                    className={`text-md p-2 hover:bg-zinc-50 delay-50 transition-colors first:rounded-t-md last:rounded-b-md ${
-                      (wordIndex + meaningIndex + definitionIndex) % 2 === 0
-                        ? "bg-zinc-300"
-                        : "bg-zinc-200"
-                    }`}
-                  >
-                    <p>
-                      [{wordIndex}.{meaningIndex}.{definitionIndex}]:{" "}
-                      {definition}
-                    </p>
-                  </div>
-                ))}
+                {definitions &&
+                  Array.isArray(definitions) &&
+                  definitions.map(({ definition }: Definition, definitionIndex: number) => (
+                    <div
+                      key={definitionIndex}
+                      className={`text-md p-2 hover:bg-zinc-50 delay-50 transition-colors first:rounded-t-md last:rounded-b-md ${
+                        (wordIndex + meaningIndex + definitionIndex) % 2 === 0
+                          ? "bg-zinc-300"
+                          : "bg-zinc-200"
+                      }`}
+                    >
+                      <p>
+                        [{wordIndex}.{meaningIndex}.{definitionIndex}]:{" "}
+                        {definition}
+                      </p>
+                    </div>
+                  ))}
               </div>
             ))}
           </div>
